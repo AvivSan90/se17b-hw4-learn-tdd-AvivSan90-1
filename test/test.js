1a164b52ea6d0f9b8400ed8935cb8b373f2e1069
AvivSan90 <avivsanilevich@gmail.com>
2017-05-14 13:36:48 +0300

    Green

--- test/test.js
+++ test/test.js
@@ -1,17 +1,29 @@
 var assert = require("assert"); // core module
 var C = require('../cash.js');  // our module
 
+var C = {};     // C Object simplifies exporting the module
+C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
     'use strict';
-    return [50, 20, 20];    // just enough to pass :-)
+    var change = [];
+    var length = C.coins.length;
+    var remaining = cashPaid - totalPayable;          // we reduce this below
+
+    for (var i = 0; i < length; i++) { // loop through array of notes & coins:
+        var coin = C.coins[i];
+
+        if(remaining/coin >= 1) { // check coin fits into the remaining amount
+            var times = Math.floor(remaining/coin);        // no partial coins
+
+            for(var j = 0; j < times; j++) {     // add coin to change x times
+                change.push(coin);
+                remaining = remaining - coin;  // subtract coin from remaining
+            }
+        }
+    }
+    return change;
 };
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    if(totalPayable == 486 && cashPaid == 1000)
-        return [500, 10, 2, 2];
-    else if(totalPayable == 210 && cashPaid == 300)
-        return [50, 20, 20];
-};
+module.exports = C;            // export the module with a single method
 
 describe('Cash Register', function(){
   describe('Module C', function(){
@@ -24,6 +36,9 @@ describe('Cash Register', function(){
 })
     it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
-    })
+})
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+});
   })
 });  
