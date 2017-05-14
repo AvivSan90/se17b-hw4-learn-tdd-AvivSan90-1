218c704f802a09c56a22ccd13beb2db65bf6bec4
AvivSan90 <avivsanilevich@gmail.com>
2017-05-14 13:32:33 +0300

    Red

--- test/test.js
+++ test/test.js
@@ -12,6 +12,9 @@ describe('Cash Register', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
     })
+    it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+})
     it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
     })
